9a87892412f4ba00f52f657d3dcd160200aa18e3
Julien <julien.jehl@jouvencia.net>
2021-08-09 12:22:55 +0200

    add sh init

---
+++ init.sh
@@ -0,0 +1,20 @@
+sudo apt update
+export DEBIAN_FRONTEND=noninteractive
+
+
+## Get pip : https://pip.pypa.io/en/stable/installing/ ##############################
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+#------------------------------------------------------------------------------------
+
+# INSTALL MINIMAL DEPENDENCIES ############################################################
+sudo pip3 install docker python-crontab
+
+
+########## swapoff
+sudo -- bash -c 'echo "swapoff -a" >> /etc/systemd/nvzramconfig.sh'
+
+
+######### give reboot privilege ###############
+sudo -- bash -c 'echo "nnvision ALL=(root) NOPASSWD: /sbin/reboot" >  /etc/sudoers.d/reboot_privilege'
+
