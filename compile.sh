a3559a96b12f2e8e8de7759c15ae787ba8daac23
Julien <julien.jehl@jouvencia.net>
2021-08-09 13:36:45 +0200

    add ffmpeg

--- compile.sh
+++ compile.sh
@@ -1,11 +1,12 @@
 cd jetson-ffmpeg
-git apply ffmpeg_nvmpi.patch
-./configure --enable-nvmpi --enable-nonfree --enable-shared --enable-gpl --enable-libx264 --cc="gcc -fPIC"
+cmake ..
 make -j4
 make install
 ldconfig
 
 cd ffmpeg
+git apply ffmpeg_nvmpi.patch
+./configure --enable-nvmpi --enable-nonfree --enable-shared --enable-gpl --enable-libx264 --cc="gcc -fPIC"
 make -j4
 make install
 ldconfig
