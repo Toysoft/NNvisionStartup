1ccd46d5627243f1b91f4f0bbeb5477217123365
Yannis <yannis.bounia@outlook.com>
2021-09-15 12:05:31 +0200

    Fix: Screensaver

--- initUx.sh
+++ initUx.sh
@@ -51,7 +51,12 @@ chmod -x $(type -p gnome-keyring-daemon)
 crudini --set /etc/gdm3/custom.conf daemon AutomaticLoginEnable true
 crudini --set /etc/gdm3/custom.conf daemon AutomaticLogin $1
 sed -i -r "s/(\S*)\s*=\s*(.*)/\1=\2/g" /etc/gdm3/custom.conf
+
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+
+mv /usr/bin/gnome-keyring-daemon /usr/bin/gnome-keyring-daemon.bak
 
 #Set up des permissions docker pour lancer initService.py
 groupadd docker
