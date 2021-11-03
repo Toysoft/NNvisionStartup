9b48c73795447836bc06fd4b24c9414b7ae06101
Julien <julien.jehl@jouvencia.net>
2021-11-03 13:56:14 +0100

    adapt for amd64 machine

---
+++ amd64/initProtecia_amd64.sh
@@ -0,0 +1,35 @@
+sudo apt update
+export DEBIAN_FRONTEND=noninteractive
+sudo  apt install -y nano curl python3-testresources
+
+## Get pip : https://pip.pypa.io/en/stable/installing/ ##############################
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+#------------------------------------------------------------------------------------
+
+# INSTALL MINIMAL DEPENDENCIES ############################################################
+sudo pip3 install docker python-crontab
+
+
+########## swapoff
+if ! grep "vm.swappiness=1" /etc/sysctl.conf
+then
+    sudo -- bash -c 'echo "vm.swappiness=1" >> /etc/sysctl.conf'
+fi
+
+######### give reboot privilege ###############
+sudo -- bash -c 'echo "nnvision ALL=(root) NOPASSWD: /sbin/reboot" >  /etc/sudoers.d/reboot_privilege'
+
+######### give nnvision docker permission ###############
+sudo -- bash -c "usermod -aG docker nnvision"
+
+#########
+mkdir /home/nnvision/conf
+touch /home/nnvision/conf/settingslocal.py
+echo "
+INIT_PASS = 'gOX8983zqQg'
+SERVER_WS = 'wss://dev.protecia.com/'
+SERVER = 'https://dev.protecia.com/'
+" >/home/nnvision/conf/settingslocal.py
+
+touch /home/nnvision/conf/__init__.py
---
+++ amd64/initService_amd64.py
@@ -0,0 +1,88 @@
+import json
+import os
+import docker
+from crontab import CronTab
+from docker.errors import APIError
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+
+# FIRST PART IS TO RUN THE GOOD CONTAINER VERSION ----------------------------------------------------------------------
+# get the version of the image to run
+def update_docker(docker_conf):
+    # test if container nnvision is running
+    client = docker.from_env()
+    # test if container nnvision is running
+    if not client.containers.list(filters={'name': 'nnvision'+str(docker_conf['docker_version'])}):
+        logging.warning('good container is not running')
+        # stop all containers
+        for c in client.containers.list():
+            c.stop()
+        client.containers.prune()
+        # test if image is existing
+        try:
+            client.images.get('roboticia/nnvision_client_amd64:'+str(docker_conf['docker_version']))
+            logging.warning(f'image {"roboticia/nnvision_client_amd64:"+str(docker_conf["docker_version"])} is OK')
+        except docker.errors.ImageNotFound:
+            # pull the image
+            logging.warning(f'Pulling the image {str(docker_conf["docker_version"])}')
+            client.login(username='nnvisionpull',
+                         password='jouvenciaprotecia69',
+                         email='dockerpull@roboticia.com',
+                         registry='https://index.docker.io/v1/')
+            client.images.pull("nnvision_client_amd64:"+str(docker_conf['docker_version']))
+        # run nnvision
+        client.containers.run(
+            "roboticia/nnvision_client_amd64:"+str(docker_conf['docker_version']),
+            restart_policy={"Name": "on-failure"},
+            entrypoint='/NNvision/python_client/start.sh',
+            name='nnvision'+str(docker_conf['docker_version']),
+            network_mode='host',
+            runtime='nvidia',
+            environment=["NVIDIA_VISIBLE_DEVICES=all", "NVIDIA_DRIVER_CAPABILITIES=compute,utility,video"],
+            volumes={'/home/nnvision/conf': {'bind': '/NNvision/python_client/conf',
+                                             'mode': 'rw'},
+                     'nn_camera': {'bind': '/NNvision/python_client/camera',
+                                   'mode': 'rw'},
+                     '/home/nnvision/uuid': {'bind': '/NNvision/uuid',
+                                                  'mode': 'rw'},
+                     },
+            detach=True,
+                              )
+        # clean unused images
+        client.images.prune()
+    logging.warning('good container is running Noting special to do')
+
+
+# SECOND PART IS TO CHECK IF REBOOT ------------------------------------------------------------------------------------
+def reboot(docker_conf):
+    logging.warning('enter reboot')
+    if docker_conf['reboot']:
+        logging.warning('do reboot')
+        os.system('sudo reboot')
+
+
+# THIRD PART IS TO INSTALL CRON (only important for first run, in case of change manually remove the installed cron) ---
+def install_update_docker_cron():
+    cron = CronTab(user=True)
+    if not any(cron.find_command('initService')):
+        cmd = "sleep 200 && cd /home/nnvision/NNvisionStartup && "
+        cmd += 'python3 /home/nnvision/NNvisionStartup/initService.py'
+        cmd += " > /home/nnvision/cron_service.log 2>&1&"
+        job = cron.new(command=cmd)
+        job.minute.every(2)
+        cron.write()
+
+
+if __name__ == "__main__":
+    conf = {'docker_version': 1.0, 'reboot': False}
+    try:
+        with open("../conf/docker.json") as version:
+            conf = json.load(version)
+    except FileNotFoundError:
+        logging.warning('can not find the docker.json file')
+        pass
+    update_docker(conf)
+    reboot(conf)
+    install_update_docker_cron()
