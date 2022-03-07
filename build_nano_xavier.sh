9f0ad487541e0adef58a7d0b4dcb1dfbcefda482
Julien <julien.jehl@jouvencia.net>
2022-03-07 14:59:21 +0100

    syntax

--- build_nano_xavier.sh
+++ build_nano_xavier.sh
@@ -1,1 +1,1 @@
-docker build --file Dockerfile_nano --rm -t roboticia/nnvision_jetson_nano_xavier:base .
+docker build --file Dockerfile_nano_xavier --rm -t roboticia/nnvision_jetson_nano_xavier:base .
