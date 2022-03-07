23b656fcf5024455667ac75e80b93d261de5b0b3
Julien <julien.jehl@jouvencia.net>
2022-03-07 18:12:51 +0100

    syntax

--- start_compile.sh
+++ start_compile.sh
@@ -4,4 +4,4 @@ docker run  -it --entrypoint /bin/bash --gpus=all --name nnvision --net=host \
             -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  \
             -v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api \
             -v /proc/device-tree/chosen:/NNvision/uuid \
-            --rm roboticia/nnvision_jetson_nano:base
+            --rm roboticia/nnvision_jetson_nano_xavier:base
